0ef834e8e914e326447f5d1a4bc8169eb7406110
Jonatan Flodin Stenholm <jonatan.flodin@hotmail.com>
2019-07-02 11:46:44 +0200

    startView and gameView done, navigation too

--- app/src/main/java/com/example/thirty/MainActivity.java
+++ app/src/main/java/com/example/thirty/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.thirty;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //playGame();
+
     }
+
+    public void playGame(View view){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
